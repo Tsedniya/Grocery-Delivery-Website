e0c23e246eb48629a5be26275dedb3123d489ab5
Tsedniya <ktsedniya@gmail.com>
2025-08-10 12:17:11 +0300

    Frontend : navigation component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
-
-export const App = () => {
+import Navbar from './components/Navbar'
+const App = () => {
   return (
     <div>
-      
-      App
+
+      <Navbar/>
 
     </div>
   )
 }
+export default App;
