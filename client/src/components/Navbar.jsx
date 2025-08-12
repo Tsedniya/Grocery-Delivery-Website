0f9ffa25eca215ec2a5fbb453257d097dd9b312e
Tsedniya <ktsedniya@gmail.com>
2025-08-12 21:06:16 +0300

    Frontend:pages

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import { useState } from 'react'
 import { NavLink } from 'react-router-dom'
 import { assets }  from '../assets/assets'
 import { useAppContext } from '../context/AppContext'
@@ -6,7 +6,7 @@ import { useAppContext } from '../context/AppContext'
 
 const Navbar = () => {
     const [open, setOpen] = useState(false);
-    const [user, setUser,setShowUserLogin,navigate] = useAppContext();
+    const {user, setUser,setShowUserLogin,navigate} = useAppContext();
     const logout = async ()=>{
           setUser(null);
           navigate('/')
@@ -16,7 +16,7 @@ const Navbar = () => {
   
          <nav className="flex items-center justify-between px-6 md:px-16 lg:px-24 xl:px-32 py-4 border-b border-gray-300 bg-white relative transition-all">
 
-            <NavLink to= '/'>
+            <NavLink to= '/' onClick={()=> setOpen(false)}>
                 <img className="h-9" src={assets.logo.png} alt="logo"/>
             </NavLink>
 
@@ -31,14 +31,23 @@ const Navbar = () => {
                     <img src={assets.search_icon} alt='search' className='w-4 h-2/4'/>
                 </div>
 
-                <div className="relative cursor-pointer">
+            <div onClick={()=>navigate("/cart")}  className="relative cursor-pointer">
                     <img src={assets.nav_cart_icon} alt='cart' className='w-6 opacity-80'/>
                     <button className="absolute -top-2 -right-3 text-xs text-white bg-primary w-[18px] h-[18px] rounded-full">3</button>
-                </div>
+            </div>
 
-                <button className="cursor-pointer px-8 py-2 bg-primary hover:bg-primary-dull transition text-white rounded-full">
+                { !user ? (<button onClick= {()=> setShowUserLogin(true)} className="cursor-pointer px-8 py-2 bg-primary hover:bg-primary-dull transition text-white rounded-full">
                     Login
-                </button>
+                </button>):(
+                    <div className='relative group'>
+                       <img src={assets.profile_icon} className='w-10' alt="profile"/>
+                       <ul className='hidden group-hover:block absolute top-10 right-0 bg-white shadow
+                         border border-gray-200 py-2.5 w-30 rounded-md text-sm z-40'>
+                         <li onClick= {()=> navigate("my-orders")} className='p-1.5 pl-3 hover:bg-primary/10 cursor-pointer'>My Orders</li>
+                         <li onClick= {logout} className='p-1.5 pl-3 hover:bg-primary/10 cursor-pointer'>Logout</li>
+                       </ul>
+                    </div>
+                )}
             </div>
 
             <button onClick={() => open ? setOpen(false) : setOpen(true)} aria-label="Menu" className="sm:hidden">
@@ -47,11 +56,14 @@ const Navbar = () => {
             </button>
 
             {/* Mobile Menu */}
+
+           {open &&(
             <div className={`${open ? 'flex' : 'hidden'} absolute top-[60px] left-0 w-full bg-white shadow-md py-4 flex-col items-start gap-2 px-5 text-sm md:hidden`}>
                 <NavLink to="/" onClick={()=> setOpen(false)}>Home</NavLink>
                 <NavLink to="/products" onClick={()=> setOpen(false)}>All products</NavLink>
-                
+
                 {user && 
+
                 <NavLink to="/products" onClick={()=> setOpen(false)}>My Orders</NavLink>
                 }
                 <NavLink to='/' onClick={()=> setOpen(false)}>Contact</NavLink>
@@ -72,7 +84,7 @@ const Navbar = () => {
                     Logout
                     </button> 
                 )}
-            </div>
+            </div> )} 
 
         </nav>
 
--- client/src/context/AppContext.jsx
+++ client/src/context/AppContext.jsx
@@ -1,13 +1,14 @@
-import { createContext,useState, useContext } from "react"
+import { createCogitntext,useState, useContext } from "react"
 import { useNavigate } from "react-router-dom";
 
 export const AppContext = createContext();
 
 export const AppContextProvider = ({children}) =>{
+    
     const navigate = useNavigate();
     const [user, setUser]= useState(null);
     const [isSeller, setIsSeller]= useState(false);
-    const [showUserLogin, setShowUserLogin] = usestate(false)
+    const [showUserLogin, setShowUserLogin] = useState(false)
    
     
     const value = {navigate, user, setUser,isSeller, setIsSeller,showUserLogin,setShowUserLogin
