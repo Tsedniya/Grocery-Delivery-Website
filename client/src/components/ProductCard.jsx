0144f610fcd07100e3112fc911314f82bea97de8
Tsedniya <ktsedniya@gmail.com>
2025-09-16 11:01:49 +0300

    Frontend:Productdetail

--- client/src/components/ProductCard.jsx
+++ client/src/components/ProductCard.jsx
@@ -3,14 +3,15 @@ import { assets } from '../assets/assets';
 import { useAppContext } from '../context/AppContext';
 
 const ProductCard= ({product}) => {
-    const [count, setCount] = React.useState(0);
+   
     const {currency, addToCart, removeFromCart, cartItems, navigate } = useAppContext()
 
 
     return product && (
-        <div className="border border-gray-500/20 rounded-md md:px-4 px-3 py-2 bg-white min-w-56 max-w-56 w-full">
+        <div onClick={()=> {navigate(`/products/${product.category.toLowerCase()}/${product._id}`); scroll(0,0)}} className="border border-gray-500/20 rounded-md md:px-4 px-3 py-2 bg-white min-w-56 max-w-56 w-full">
             <div className="group cursor-pointer flex items-center justify-center px-2">
-                <img className="group-hover:scale-105 transition max-w-26 md:max-w-36" src={product.image[0]} alt={product.name} />
+                <img className="group-hover:scale-105 transition max-w-26 md:max-w-36" 
+                src={product.image[0]} alt={product.name} />
             </div>
             <div className="text-gray-500/60 text-sm">
                 <p>{product.category}</p>
@@ -18,16 +19,17 @@ const ProductCard= ({product}) => {
                 <div className="flex items-center gap-0.5">
                     {Array(5).fill('').map((_, i) => (
                        
-                           <img key = {i} className='md:w-3.5 w3' src={i < 4 ? assets.start_icon: assets.star_dull_icon} alt=""/>
+                           <img key = {i} className='md:w-3.5 w3' src={i < 4 ? assets.star_icon: assets.star_dull_icon} alt=""/>
                       
                     ))}
                     <p>(4)</p>
                 </div>
                 <div className="flex items-end justify-between mt-3">
                     <p className="md:text-xl text-base font-medium text-primary">
-                        {currency}${product.offerPrice}{""} <span className="text-gray-500/60 md:text-sm text-xs line-through">${currency}${product.price}</span>
+                        {currency}${product.offerPrice}{" "} <span className="text-gray-500/60 md:text-sm text-xs line-through">
+                        {currency}${product.price}</span>
                     </p>
-                    <div onClick={(e)=>{e.stopPropagation();}} className="text-primary">
+                    <div onClick={(e)=> {e.stopPropagation();}} className="text-primary">
                         {!cartItems[product._id] ? (
                             <button className="flex items-center justify-center gap-1 bg-primary/10 border border-primary/40 md:w-[80px] w-[64px] h-[34px] rounded cursor-pointer" onClick={() => addToCart(product._id)} >
                               <img src={assets.cart_icon} alt = "cart_icon"   />
--- client/src/pages/ProductCatagory.jsx
+++ client/src/pages/ProductCatagory.jsx
@@ -21,7 +21,7 @@ const ProductCatagory = () => {
         )} 
         {filteredProducts.length > 0 ?(
           <div className='grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-3 md:gap-6 mt-6'>
-            {filteredProducts.map((proucts)=>(
+            {filteredProducts.map((product)=>(
               <ProductCard key ={product._id} product={product}/>            ))}
           </div>
             ):(
