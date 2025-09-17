c3a0e6cf637299e06680270472944f7bf65a2f48
Tsedniya <ktsedniya@gmail.com>
2025-09-17 14:04:54 +0300

    Frontend:AddAddress

---
+++ client/src/pages/AddAddress.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const inputField = (()=>(
+    <input type="text"/>
+))
+
+const AddAddress = () => {
+  return (
+    <div>AddAddress</div>
+  )
+}
+
+export default AddAddress
